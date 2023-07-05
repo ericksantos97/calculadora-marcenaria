6fb8b1d76082613ce322b88ba6d472bfa85fe927
Erick Santos <erick.tec97@gmail.com>
2023-07-05 18:01:18 -0300

    feat(x): teste construcao barra de pesquisa angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+export interface User {
+  nome: string;
+  cpf: string;
+  codigoCarteira: string;
+  nomeCarteira: string;
+  data: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +18,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadora-marcenaria';
+
+  myControl = new FormControl('');
+  options: User[] = [
+    { nome: 'Carlos1', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+    { nome: 'Carlos2', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+    { nome: 'Carlos3', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+    { nome: 'Carlos4', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+    { nome: 'Carlos5', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+    { nome: 'Carlos6', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+    { nome: 'Carlos7', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+    { nome: 'Carlos8', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+    { nome: 'Carlos9', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+    { nome: 'Carlos10', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+    { nome: 'Carlos11', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+    { nome: 'Carlos12', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+    { nome: 'Carlos13', cpf: '45612378903', codigoCarteira: '003', nomeCarteira: 'Carteira 3', data: '10/03/2023' },
+  ];
+  filteredOptions!: Observable<User[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        let inputValue = '';
+
+        if (value) { inputValue = (typeof value === 'string') ? value : value['nome']; }
+
+        if (inputValue && inputValue.length >= 3) {
+          const filterValue = inputValue.toLowerCase();
+          const filteredItems = this.options.filter((option: User) =>
+            option.nome.toLowerCase().includes(filterValue)
+          );
+          return filteredItems.slice(0, 10).map((item, index) => ({
+            ...item,
+            isLastOption: index === filteredItems.length - 1
+          }));
+        } else {
+          return [];
+        }
+      })
+    );
+  }
+
+  displayFn(user: User): string {
+    return user && user.nome ? user.nome : '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -13,6 +18,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt' },
